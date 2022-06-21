44b15f5a4bc7a6486b9774958c0d0acd53dcf78f
xinyuanzheng <xinyuanzheng1995@gmail.com>
2022-06-21 19:47:47 -0400

    Complete updating a todo

--- datastore/index.js
+++ datastore/index.js
@@ -68,13 +68,19 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
-  if (!item) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    items[id] = text;
-    callback(null, { id, text });
-  }
+  fs.readFile(`${exports.dataDir}/${id}.txt`, 'utf8', (err, fileData) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, err => {
+        if (err) {
+          callback(new Error('Error writing file'));
+        } else {
+          callback(null, { id, text });
+        }
+      });
+    }
+  });
 };
 
 exports.delete = (id, callback) => {
